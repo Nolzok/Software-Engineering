f2ec581ef9fcc6c09b881cd26e749f0e237b3be6
Elenironto <165507779+Elenironto@users.noreply.github.com>
2025-05-22 15:32:51 +0300

    Update event.py

--- Code/Project-Code-v1.0/models/event.py
+++ Code/Project-Code-v1.0/models/event.py
@@ -5,19 +5,12 @@ class Event:
         self.event_id = event_id
         self.details = details
         self.rating = rating
-        self.categories = categories
+        self.category = category
         self.organizer_id = organizer_id
         self.reviews = []
 
-    def get_reviews(self):
-        return self.reviews
-
     def load_past_events(self):
-        # Θα επιστρέψει dummy δεδομένα
         return []
-
-    def add_review(self, review):
-        self.reviews.append(review)
 
     def display_details_of_events(self):
         print(f"{self.name} - {self.details}")
