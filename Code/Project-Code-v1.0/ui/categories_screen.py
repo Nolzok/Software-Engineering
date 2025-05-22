4c72ce4e574289203c8d11aacaafc64acb4ce6d8
Elenironto <165507779+Elenironto@users.noreply.github.com>
2025-05-22 15:13:17 +0300

    Add files via upload

---
+++ Code/Project-Code-v1.0/ui/categories_screen.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from ui.category_screen import show_category_screen
+from ui.utils import center_window
+
+
+def show_categories_screen(root, client):
+    categories_window = tk.Toplevel(root)
+    categories_window.title("Κατηγορίες Ανταμοιβών")
+    categories_window.geometry("600x500")
+    center_window(categories_window)
+    categories_window.config(bg="#22A298")
+
+    top_frame = tk.Frame(categories_window, bg="#166A64")
+    top_frame.pack(fill=tk.X, pady=5)
+
+    tk.Label(top_frame, text="Κατηγορίες Ανταμοιβών", font=("Arial", 14, "bold"),
+             bg="#166A64", fg="white").pack(side=tk.LEFT, padx=10)
+
+    tk.Label(categories_window, text="Επίλεξε Κατηγορία", font=("Arial", 14, "bold"),
+             bg="#22A298", fg="black").pack(pady=1)
+
+    def on_select(category):
+        show_category_screen(root, client, category)
+        categories_window.destroy()
+
+    categories = [
+        "Αθλητισμός", "Διασκέδαση", "Τεχνολογία", "Πολιτισμός", "Μουσική",
+        "Εκπαίδευση", "Τέχνη", "Επιχειρηματικότητα", "Υγεία & Ευεξία"
+    ]
+
+    for category in categories:
+        tk.Button(categories_window, text=category, width=25,
+                  font=("Arial", 12, "bold"),
+                  bg="black", fg="#DDB0B0",
+                  command=lambda c=category: on_select(c)).pack(pady=10)
