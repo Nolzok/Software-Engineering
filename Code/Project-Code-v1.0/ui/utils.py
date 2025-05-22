176eea284ab7f0f850aa520cfa8c127cb581b1bd
Elenironto <165507779+Elenironto@users.noreply.github.com>
2025-05-22 15:11:52 +0300

    Add files via upload

---
+++ Code/Project-Code-v1.0/ui/utils.py
@@ -0,0 +1,7 @@
+def center_window(window, width=600, height=500):
+    window.update_idletasks()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = int((screen_width - width) / 2)
+    y = int((screen_height - height) / 2)
+    window.geometry(f"{width}x{height}+{x}+{y}")
