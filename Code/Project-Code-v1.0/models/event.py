5bd6a3f1e2b6435cbf0c859daa111f36eed9c602
Elenironto <165507779+Elenironto@users.noreply.github.com>
2025-05-23 17:14:41 +0300

    Update event.py

--- Code/Project-Code-v1.0/models/event.py
+++ Code/Project-Code-v1.0/models/event.py
@@ -1,16 +1,34 @@
+from datetime import datetime
+
 class Event:
-    def __init__(self, name, date, event_id, details, rating, categories, organizer_id):
+    def __init__(self, name: str, date: datetime,
+                 location: str = "", max_participants: int = 0,
+                 category: str = "", price: float = 0.0,
+                 duration: int = 0, description: str = "",
+                 event_id: str = None, rating: float = 0.0,
+                 organizer_id: int = None):
         self.name = name
         self.date = date
-        self.event_id = event_id
-        self.details = details
-        self.rating = rating
+        self.location = location
+        self.max_participants = max_participants
         self.category = category
+        self.price = price
+        self.duration = duration
+        self.description = description
+
+        self.event_id = event_id
+        self.rating = rating
         self.organizer_id = organizer_id
-        self.reviews = []
 
-    def load_past_events(self):
-        return []
-
-    def display_details_of_events(self):
-        print(f"{self.name} - {self.details}")
+    def display_details_of_event(self):
+        return {
+            "name": self.name,
+            "date": self.date,
+            "location": self.location,
+            "max_participants": self.max_participants,
+            "category": self.category,
+            "price": self.price,
+            "duration": self.duration,
+            "description": self.description,
+            "rating": self.rating
+        }
