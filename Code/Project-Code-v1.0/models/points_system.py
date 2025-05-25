68bf418ed4620158b50dd79254852819668dd8b0
Elenironto <165507779+Elenironto@users.noreply.github.com>
2025-05-25 22:34:42 +0300

    Add files via upload

---
+++ Code/Project-Code-v1.0/models/points_system.py
@@ -0,0 +1,21 @@
+class PointsSystem:
+    def __init__(self):
+        self.total_points = 0
+        self.extra_points = 0
+
+    def calculate_points(self):
+        self.total_points += self.extra_points
+        print("Υπολογίστηκαν οι πόντοι.")
+
+    def show_points(self):
+        print(f"Πόντοι: {self.total_points}")
+        return self.total_points
+
+    def check_balance(self):
+        return self.total_points >= 100
+
+    def deduct_points(self, amount):
+        if self.total_points >= amount:
+            self.total_points -= amount
+            return True
+        return False
