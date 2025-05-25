b2c305a4ff4782f63b8966235f2a1421879915f0
Giovanni <146768034+Nolzok@users.noreply.github.com>
2025-05-25 22:28:54 +0300

    Delete Code/Project-Code-v1.0/util directory

--- Code/Project-Code-v1.0/util/validators.py
+++
@@ -1,7 +0,0 @@
-def validate_form(form):
-    return all([
-        hasattr(form, 'full_name') and form.full_name.strip(),
-        hasattr(form, 'age') and str(form.age).strip(),
-        hasattr(form, 'phone_number') and form.phone_number.strip(),
-        hasattr(form, 'email') and form.email.strip()
-    ])
